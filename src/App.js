fe51e8bcc81296278a3c4708685a14eaa1bb57e3
Adrian1426 <adrianhh1426@gmail.com>
2020-07-22 16:53:02 -0500

    setState con una función que mantiene el state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import React from 'react';
-import Fruta from './components/fruta';
 import './App.css';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Fruta
-        nombre='Manzana'
-        precio={10}
-      />
+class App extends React.Component {
 
-      <Fruta
-        nombre='Mango'
-        precio={15}
-      />
-    </div>
-  );
+  state = {
+    click: 0
+  };
+
+  addClick = () => {
+    this.setState({
+      click: this.state.click + 1
+    });
+  };
+
+  addClick2 = () => {
+    this.setState((prevState, props) => {
+      if (prevState.click === 5) {
+        return null;
+      }
+
+      return {
+        click: prevState.click + 1
+      }
+
+    });
+  };
+
+  addClick3 = () => {
+    this.setState(prevState => (
+      {
+        click: prevState.click + 1
+      }
+    ));
+  };
+
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.addClick2}>clicks ({this.state.click})</button>
+      </div>
+    );
+  }
 }
 
 export default App;
