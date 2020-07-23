25df70b04c51d3669b11bb65b20ef822acd8982c
Adrian1426 <adrianhh1426@gmail.com>
2020-07-22 19:54:22 -0500

    persistencia de eventos

--- src/App.js
+++ src/App.js
@@ -1,38 +1,47 @@
 import React from 'react';
 import './App.css';
 
-const Gato = props => {
-  return (
-    <div>
-      <h1>Gato</h1>
-      <pre>{JSON.stringify(props, null, 4)}</pre>
-    </div>
-  );
-};
 
-class App extends React.Component {
+
+class PersistenciaEventos extends React.Component {
   state = {
-    vidas: 6,
-    hijos: 9,
+    color: 'blue'
+  };
+
+  onChangeInput = (e) => {
+
+    const color = e.target.value;
+
+    // e.persist();
+
+    // this.setState({
+    //   color: e.target.value
+    // });
+
+    this.setState(() => ({
+      color
+    }));
   };
 
   render() {
-    const otrosDatos = {
-      color: 'negro',
-      dueño: 'Adrian',
-    };
 
     return (
-      <div className="App">
-        <Gato
-          nombre="Garfiel"
-          edad={5}
-          {...otrosDatos}
-          {...this.state}
-        />
+      <div>
+        <input type="text" onChange={this.onChangeInput} />
+        <h1 style={{ color: this.state.color }}>{this.state.color}</h1>
       </div>
     );
   }
 }
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <PersistenciaEventos />
+    </div>
+  );
+};
+
 
 export default App;
